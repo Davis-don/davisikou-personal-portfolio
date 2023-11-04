668ed8bc8dd3f304bd3fb88d340c8d04f8354a63
Davis-don <as26111632020@student.mut.ac.ke>
2023-11-04 10:17:00 -0700

    made the skills,expertise and education links active

--- src/Aboutsection.js
+++ src/Aboutsection.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Aboutsection.css'
 import donimage from './Images/donieimg.jpg'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {FaUserGraduate} from "react-icons/fa"
 
 
 function Userimage() {
@@ -25,13 +26,45 @@ function Aboutcontent() {
   )
 }
 function Navigationlinks(){
+  const [Isskill,Setskill]=useState(true);
+  const [IsExpertise,SetExpertise]=useState();
+  const [Iseducation,Seteducation]=useState()
+
+  const Handleclicks=(status)=>{
+    if(status=='skillactive'){
+      SetExpertise(IsExpertise=>false);
+      Seteducation(Iseducation=>false);
+      Setskill(Isskill=>true);
+    var skill=true;
+    var Expertise=false;
+    var Education=false;
+    }
+    else if(status=='Expertiseactive'){
+      Setskill(Isskill=>false)
+      SetExpertise(IsExpertise=>true)
+      Seteducation(Iseducation=>false)
+    }
+    else if(status=='Educationactive'){
+      Setskill(Isskill=>false)
+      SetExpertise(IsExpertise=>false)
+      Seteducation(Iseducation=>true)
+    }
+   }
     return(
         <div className='Skills-section container-fluid'>
         <ul className='list-unstyled skills-links'>
-            <li className='text-decoration-none text-light active-skill'>Skills</li>
-            <li className='text-decoration-none text-light'>Expertise</li>
-            <li className='text-decoration-none text-light'>Education</li>
+            <li  style={{
+          borderBottom: Isskill ? '1px solid red' : '',
+        }} className='text-decoration-none text-light' onClick={()=>Handleclicks('skillactive')}>Skills</li>
+            <li    style={{
+          borderBottom: IsExpertise ? '1px solid red' : '',
+        }} className='text-decoration-none text-light' onClick={()=>Handleclicks('Expertiseactive')}>Expertise</li>
+            <li    style={{
+          borderBottom: Iseducation ? '1px solid red' : '',
+        }} className='text-decoration-none text-light'onClick={()=>Handleclicks('Educationactive')}>Education</li>
         </ul>
+        {
+        Isskill && 
         <div className='skills-description'>
             <ul className='list-unstyled skills-display'>
                 <li style={{lineHeight:'5px'}}>
@@ -44,6 +77,33 @@ function Navigationlinks(){
                 </li>
             </ul>
             </div>
+               }
+               {
+                IsExpertise && 
+            <div className='Expertise-desription'>
+            <ul className='list-unstyled skills-display'>
+                <li style={{lineHeight:'5px'}}>
+                    <p style={{color:' #F30606'}}>HTML/CSS</p>
+                    <p className='text-light'>To display and style web content</p>
+                </li>
+                <li style={{lineHeight:'5px'}}>
+                    <p style={{color:' #F30606'}}>Bootstrap 5</p>
+                    <p className='text-light'>For responsive webpage designs</p>
+                </li>
+                <li style={{lineHeight:'5px'}}>
+                    <p style={{color:' #F30606'}}>Javascript</p>
+                    <p className='text-light'style={{lineHeight:'20px'}}>With its liblaries such as react for scripting</p>
+                </li>
+            </ul>
+            </div>
+                }
+                { 
+                Iseducation && 
+            <div className='Education description'>
+              <p className='text-danger'><FaUserGraduate className='text-light' /> Undergraduate</p>
+              <p className='text-light'>Student at Muranga University of Technology persing Bachelor of Science in Mathematics and Computer science.</p>
+            </div>
+            }
             </div>
     )
 }
@@ -63,3 +123,4 @@ export default function Aboutsection() {
         </div>
   )
 }
+
